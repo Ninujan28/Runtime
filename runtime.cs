176e0c75f6ebbf7afeb2f88b489f96d0fe768a2c
Ninujan28 <ninujant@gmail.com>
2023-03-23 21:28:49 +0530

    Added Runtime

---
+++ runtime.cs
@@ -0,0 +1,47 @@
+﻿cusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Random r = new Random();
+            Console.WriteLine("Input Number of Contenstant:");
+            int size=Convert.ToInt32(Console.ReadLine());
+            double [] timings = new double[size];
+            for(int i =0; i<size; i++)
+            {
+                timings[i] = 80*r.NextDouble()+20;
+                Console.WriteLine(timings[i]);
+
+            }
+            int firstIndex = 0;
+            double first= timings[0];
+            for (int j=0;j<size;j++)
+            {
+                if (timings[j]<first)
+                {
+                    first = timings[j];
+                   
+                }
+
+            }
+            Console.WriteLine("Best timing (min) is: " + first);
+
+
+
+        }
+    }
+}
+    
+
+
+
+
+
